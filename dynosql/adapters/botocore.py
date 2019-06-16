6183555d443dfd6adfe8058525e6f8ed2dc2a5d1
James Burke <james.burke1203@gmail.com>
2019-06-16 21:16:15 +0200

    reformating

--- dynosql/adapters/botocore.py
+++ dynosql/adapters/botocore.py
@@ -108,13 +108,19 @@ class BotocoreAdapter(object):
         try:
             partition_key_value, sort_key_value = key
             keys = {
-                self.tables[table_name]['partition_key'][0]: { DYNAMODB_DATATYPES_LOOKUP[self.tables[table_name]['partition_key'][1]]: str(partition_key_value) },
-                self.tables[table_name]['sort_key'][0]: { DYNAMODB_DATATYPES_LOOKUP[self.tables[table_name]['sort_key'][1]]: str(sort_key_value) }
+                self.tables[table_name]['partition_key'][0]: {
+                    DYNAMODB_DATATYPES_LOOKUP[self.tables[table_name]['partition_key'][1]]: str(partition_key_value)
+                },
+                self.tables[table_name]['sort_key'][0]: {
+                    DYNAMODB_DATATYPES_LOOKUP[self.tables[table_name]['sort_key'][1]]: str(sort_key_value)
+                }
             }
         except ValueError:
             partition_key_value, sort_key_value = (key, None,)
             keys = {
-                self.tables[table_name]['partition_key'][0]: { DYNAMODB_DATATYPES_LOOKUP[self.tables[table_name]['partition_key'][1]]: str(partition_key_value) }
+                self.tables[table_name]['partition_key'][0]: {
+                    DYNAMODB_DATATYPES_LOOKUP[self.tables[table_name]['partition_key'][1]]: str(partition_key_value)
+                }
             }
         except TypeError:
             raise KeyError('Table was not defined with a sort key')
