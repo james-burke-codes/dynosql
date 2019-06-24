1322a0cbd5a6be4130e004966791fc9e2d0cd132
James Burke <james.burke1203@gmail.com>
2019-06-24 18:35:39 +0200

    added additional basic operations

--- dynosql/dyno_attribute.py
+++ dynosql/dyno_attribute.py
@@ -15,7 +15,27 @@ class DynoAttribute(object):
 
     def __ne__(self, value):
         logger.info('%s != %s' % (self.name, value))
-        self.query = (self.name, '!=', value)
+        self.query = (self.name, '<>', value)
+        return self.query
+
+    def __gt__(self, value):
+        logger.info('%s  %s' % (self.name, value))
+        self.query = (self.name, '>', value)
+        return self.query
+
+    def __ge__(self, value):
+        logger.info('%s  %s' % (self.name, value))
+        self.query = (self.name, '>=', value)
+        return self.query
+
+    def __lt__(self, value):
+        logger.info('%s  %s' % (self.name, value))
+        self.query = (self.name, '<', value)
+        return self.query
+
+    def __le__(self, value):
+        logger.info('%s  %s' % (self.name, value))
+        self.query = (self.name, '<=', value)
         return self.query
 
     # def __and__(self, value):
